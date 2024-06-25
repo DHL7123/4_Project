6dde19cec2107665981c8c5d2d62ffe5ce0591d2
DHL7123 <dlehdgns7701@gmail.com>
2024-06-25 16:16:38 +0900

    no message

---
+++ Evo/mainpage/mainpage/src/test/java/com/Evo/mainpage/MainpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Evo.mainpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
