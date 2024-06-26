16660e71d6db7b0e072c4066622558b09ce5d8a5
DHL7123 <dlehdgns7701@gmail.com>
2024-06-26 15:17:48 +0900

    MainController @RestController -> Controller수정 html.tempates추가

--- Evo/mainpage/mainpage/src/main/java/com/Evo/mainpage/controller/MainpageController.java
+++ Evo/mainpage/mainpage/src/main/java/com/Evo/mainpage/controller/MainpageController.java
@@ -2,6 +2,8 @@
 package com.Evo.mainpage.controller;
 
 import com.Evo.mainpage.model.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * MainpageController 컨트롤러는 /showProduct에 대한 GET 요청을 처리.
  */
 
-@RestController
+@Controller
 public class MainpageController {
 
     private final RestTemplate restTemplate;
@@ -32,16 +34,18 @@ public class MainpageController {
      */
 
     @GetMapping("/showProduct")
-    public String showProduct() {
+    public String showProduct(Model model) {
         // Product URL
         String productUrl = "http://localhost:8092/products";
         try {
             // RestTemplate을 사용하여 Product에 GET 요청을 보내고, 응답을 Product 객체로 전달받음.
             Product product = restTemplate.getForObject(productUrl, Product.class);
-            return "상품명: " + product.getName() +"가격 :"+ product.getPrice();
+            model.addAttribute("product", product);
+            return "product";
         } catch (Exception e) {
             // 예외 처리. 위 url 에서 로컬호스트 포트를 잘못 설정하면 예외가 발생함.
-            return "접근 불가 " + e.getMessage();
+            model.addAttribute("errorMessage", "접근 불가: " + e.getMessage());
+            return "error";
         }
     }
 }
