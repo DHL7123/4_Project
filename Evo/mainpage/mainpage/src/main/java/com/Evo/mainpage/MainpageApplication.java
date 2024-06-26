0a7ce9bf8564f0d441be36129869d9eac70cad20
Ryang <62952298+Parktaeryang@users.noreply.github.com>
2024-06-26 11:49:43 +0900

    Revert "Develop"

--- Evo/mainpage/mainpage/src/main/java/com/Evo/mainpage/MainpageApplication.java
+++ Evo/mainpage/mainpage/src/main/java/com/Evo/mainpage/MainpageApplication.java
@@ -1,6 +1,5 @@
 package com.Evo.mainpage;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,9 +9,5 @@ public class MainpageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MainpageApplication.class, args);
 	}
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-}
 
+}
